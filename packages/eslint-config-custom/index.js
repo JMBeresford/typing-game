9eefaa727289116ed69f404e26957c7d0863e66b
John Beresford <jberesford@volcaus.com>
2023-07-04 20:53:50 -0700

    init turborepo
    - setup turborepo monorepo
    - setup storybook (workshop) app

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['next', 'turbo', 'prettier'],
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-img-element': 'off',
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
+};
