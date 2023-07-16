80eb258c6ac07032c7900e6d8ad0d05747e5ed62
John Beresford <jberesford@volcaus.com>
2023-07-15 21:15:30 -0700

    chore: bump versions, add glsl support

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,30 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui", "ecs"],
+
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g|ttf|hdr|glb|gltf)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            fallback: "file-loader",
+            name: "[name]-[hash].[ext]",
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    });
+
+    // shader support
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    return config;
+  },
 };
