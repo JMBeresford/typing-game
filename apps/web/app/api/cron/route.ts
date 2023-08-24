42a0d71a5bc512c5ad85eab1c60b85efa7a2e7c8
John Beresford <jberesford@volcaus.com>
2023-08-23 19:40:43 -0700

    chore: add cron job to keep DB active (#8)

---
+++ apps/web/app/api/cron/route.ts
@@ -0,0 +1,24 @@
+// Supabase will sleep inactive DBs on free tier after ~7days of inactivity
+// use this cron to keep the DB active and game the system >:)
+
+import { Database } from "@/lib/database.types";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import type { NextRequest } from "next/server";
+
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+export async function GET(_request: NextRequest) {
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore });
+  const { data, error, status } = await supabase.from("profiles").select().limit(1).single();
+
+  return NextResponse.json(
+    { data, error },
+    {
+      status,
+    },
+  );
+}
